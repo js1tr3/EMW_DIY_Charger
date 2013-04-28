3c1b0de943528d321b29180419b5bcd11ac9533e
Jason Siegel <siegeljb@gmail.com>
2013-04-27 21:38:42 -0400

    Modifications to work with latest Arduino 1.04

--- uLCD_144/uLCD_144/uLCD_144.cpp
+++ uLCD_144/uLCD_144/uLCD_144.cpp
@@ -7,7 +7,9 @@ uLCD_144::uLCD_144(int baud) {
   Serial.begin(baud);
   // display requires some time to init before sending auto-baud command
   delay(1000);
-  Serial.print(0x55, BYTE);
+ // Serial.print(0x55, BYTE);
+	Serial.write(0x55);
+
   waitAck();
 }
 
@@ -30,111 +32,115 @@ void uLCD_144::waitAck() {
 
 void uLCD_144::setContrast(int contrast) {
   // range from 0 to 0x0f
-  Serial.print(0x59,BYTE);
-  Serial.print(0x02,BYTE);
-  Serial.print(contrast,BYTE);
+//  Serial.print(0x59,BYTE);
+//  Serial.print(0x02,BYTE);
+//  Serial.print(contrast,BYTE);
+	Serial.write(0x59);
+	Serial.write(0x02);
+	Serial.write(contrast);
   waitAck();  
 }
 
 void uLCD_144::clrScreen() {
-  Serial.print(0x45,BYTE);
+  Serial.write(0x45);
+  
   waitAck();
 }  
   
 void uLCD_144::setBgColor(byte red, byte green, byte blue) {
-  Serial.print(0x42, BYTE); // set screen color
-  Serial.print(getMSB(red, green, blue), BYTE);
-  Serial.print(getLSB(red, green, blue), BYTE);
+  Serial.write(0x42); // set screen color
+  Serial.write(getMSB(red, green, blue));
+  Serial.write(getLSB(red, green, blue));
   waitAck();
 }
 
 void uLCD_144::setPenSize(int size) {
   // 0 = solid, 1 = wire
-  Serial.print(0x70,BYTE);
-  Serial.print(size,BYTE);
+  Serial.write(0x70);
+  Serial.write(size);
   waitAck();
 }
 
 void uLCD_144::setFont(int font) {
   // 0 = small, 1 = med, 2 = large
-  Serial.print(0x46,BYTE);
-  Serial.print(font,BYTE);
+  Serial.write(0x46);
+  Serial.write(font);
   waitAck();
 }
 
 void uLCD_144::setOpacity(int opacity) {
   // 0 = transparent, 1 = opaque
-  Serial.print(0x4f,BYTE);
-  Serial.print(opacity,BYTE);
+  Serial.write(0x4f);
+  Serial.write(opacity);
   waitAck();
 }
 
 // draw pixel
 void uLCD_144::drawPixel(int x, int y, byte red, byte green, byte blue) {
-  Serial.print(0x50,BYTE);
-  Serial.print(x, BYTE);
-  Serial.print(y, BYTE);
-  Serial.print(getMSB(red, green, blue), BYTE);
-  Serial.print(getLSB(red, green, blue), BYTE);
+  Serial.write(0x50);
+  Serial.write(x);
+  Serial.write(y);
+  Serial.write(getMSB(red, green, blue));
+  Serial.write(getLSB(red, green, blue));
   waitAck();
 }
 
 // draw circle
 void uLCD_144::drawCircle(int x, int y, int r, byte red, byte green, byte blue) {
-  Serial.print(0x43,BYTE);
-  Serial.print(x, BYTE);
-  Serial.print(y, BYTE);
-  Serial.print(r, BYTE);
-  Serial.print(getMSB(red, green, blue), BYTE);
-  Serial.print(getLSB(red, green, blue), BYTE);
+  Serial.write(0x43);
+  Serial.write(x);
+  Serial.write(y);
+  Serial.write(r);
+  Serial.write(getMSB(red, green, blue));
+  Serial.write(getLSB(red, green, blue));
   waitAck();
 }
 
 void uLCD_144::printStr(int col, int row, int font, byte red, byte green, byte blue, const char *str) {
-  Serial.print(0x73,BYTE);
-  Serial.print(col, BYTE);
-  Serial.print(row, BYTE);
-  Serial.print(font, BYTE);
-  Serial.print(getMSB(red, green, blue), BYTE);
-  Serial.print(getLSB(red, green, blue), BYTE);
+  Serial.write(0x73);
+  Serial.write(col);
+  Serial.write(row);
+  Serial.write(font);
+  Serial.write(getMSB(red, green, blue));
+  Serial.write(getLSB(red, green, blue));
   Serial.print(str);
-  Serial.print(0, BYTE);
+  Serial.write(0);
   waitAck();
 }
 
 void uLCD_144::printGRStr(int x, int y, int font, byte red, byte green, byte blue, int w, int h, const char *str) {
-  Serial.print(0x53,BYTE);
-  Serial.print(x, BYTE);
-  Serial.print(y, BYTE);
-  Serial.print(font, BYTE);
-  Serial.print(getMSB(red, green, blue), BYTE);
-  Serial.print(getLSB(red, green, blue), BYTE);
-  Serial.print(w, BYTE);
-  Serial.print(h, BYTE);
+  Serial.write(0x53);
+  Serial.write(x);
+  Serial.write(y);
+  Serial.write(font);
+  Serial.write(getMSB(red, green, blue));
+  Serial.write(getLSB(red, green, blue));
+  Serial.write(w);
+  Serial.write(h);
   Serial.print(str);
-  Serial.print(0, BYTE);
+  Serial.write(0);
   waitAck();
 }
 
 void uLCD_144::drawLine(int  x1, int y1, int x2, int y2, byte red, byte green, byte blue) {
-  Serial.print(0x4C,BYTE);
-  Serial.print(x1, BYTE);
-  Serial.print(y1, BYTE);
-  Serial.print(x2, BYTE);
-  Serial.print(y2, BYTE);
-  Serial.print(getMSB(red, green, blue), BYTE);
-  Serial.print(getLSB(red, green, blue), BYTE);
+  Serial.write(0x4C);
+  Serial.write(x1);
+  Serial.write(y1);
+  Serial.write(x2);
+  Serial.write(y2);
+  Serial.write(getMSB(red, green, blue));
+  Serial.write(getLSB(red, green, blue));
   waitAck();
 }
 
 void uLCD_144::drawRect(int x1, int y1, int x2, int y2, byte red, byte green, byte blue) {
-  Serial.print(0x72,BYTE);
-  Serial.print(x1, BYTE);
-  Serial.print(y1, BYTE);
-  Serial.print(x2, BYTE);
-  Serial.print(y2, BYTE);
-  Serial.print(getMSB(red, green, blue), BYTE);
-  Serial.print(getLSB(red, green, blue), BYTE);
+  Serial.write(0x72);
+  Serial.write(x1);
+  Serial.write(y1);
+  Serial.write(x2);
+  Serial.write(y2);
+  Serial.write(getMSB(red, green, blue));
+  Serial.write(getLSB(red, green, blue));
   waitAck(); 
 }
 //================================ END SCREEN FUNCTION LIBRARY ========================
